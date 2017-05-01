40c4a17d0a7bcf01604e6498bd4a43a206f0cfe2
Allen Wipf <allenwipf@msn.com>
2017-05-01 16:30:08 -0500

    fixed word counter

--- 006-word-counting.js
+++ 006-word-counting.js
@@ -1,25 +1,28 @@
+// Given a large string containing words, count how many occurrences of each word are present.
 
-countHash = {}
-function countWords(string){
+var stringOfWords = "It is rather for us, the living, we here be dedicated to the great task remaining before us.";
+var stringOfWords = stringOfWords.replace(/[^a-zA-Z 0-9]+/g,'').toLowerCase().split(" ");
 
-	var eachletter = string.split("");
+wordHash = {}
 
-	for (var x =0; x <= string.length -1; x++) {
-
-	    if !eachletter in countHash){
-
-	    	countHash = {
-
-	    		count: 1
-
-	    	}
-	    }
+for (var x = 0; x <= stringOfWords.length -1; x++){
+	var word = stringOfWords[x]
+	if (word in wordHash){
+		wordHash[word] += 1
+	} else {
+		wordHash[word] = 1
 	}
 
-	console.log(countHash);
-
 }
+ 
+console.log(wordHash);
 
+// Need key value pair
+// split words by space
 
-countWords("It is rather for us, the living, we here be dedicated to the great task remaining before us.")
+// downcase each word
+// get rid of punctuation
 
+// Iterate array of words, check against hash, if key doesn't exist, set as key and equal value to 1
+// If key does exist, increment value for that key.
+// do this until no more words exists. 
