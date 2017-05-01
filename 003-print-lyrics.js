785ec96aebcb84336db4ee28968c52674a532bce
Allen Wipf <allenwipf@msn.com>
2017-05-01 15:41:43 -0500

    finished lyrics toy problem

--- 003-print-lyrics.js
+++ 003-print-lyrics.js
@@ -1,15 +1,29 @@
 
-var days = ["first", "second"]
-var unique = ["Partridge in a Pear Tree", "Turtle Doves", "3 French Hens", "4 Calling Birds", "5 Golden Rings", "6 Geese a Laying", "7 Swans a Swimming"]
+var days = ["first", "second", "third", "forth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"]
+var unique = ["Partridge in a Pear Tree", "Turtle Doves", "French Hens", "Calling Birds", "Golden Rings", "Geese a Laying", "Swans a Swimming", "Maids a Milking", "Ladies Dancing", "Lords a Leaping", "Pipers Piping", "Drummers Drumming"]
 
-for (var x = 1; x <= 2; x++){
-
+for (var x = 1; x <= days.length; x++){
 	console.log("On the " + days[x - 1] + " of Christmas")
 	console.log("My true love gave to me")
-	console.log(x + " " + unique[x -1] + '\n')
-	console.log ('\n')
+	console.log(x + " " + unique[x -1])
 
+  for (var y = x -1; y > 0; y-- ){
+  	if (y == 1){
+  		console.log("and " + y + " " + unique[y -1])
+  	} else { 
+  		console.log(y + " " + unique[y -1])
+  	}
+  	
+  }
+
+	console.log ('\n')
 
 }
 
 
+// loop through days
+// take the value of a certain days index and match it up the same index in unique
+
+// during each iteration, iterate backward on the whatever x equals to get rest of days. 
+
+// do this until x is equal to days length.
--- 022-nokogiri.rb
+++ 022-nokogiri.rb
@@ -6,9 +6,24 @@ puts "Type in a topic to see it's opening paragraph on Wikipedia:"
 topic = gets.chomp
 
 topic = topic.gsub(" ", "_")
-
 doc = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/" + topic))
+text = doc.css("p")
+para = 0
 
-text = doc.css("p").first.text
+puts ' *********** ' + topic.upcase + ' *********** ' + \
+while para != "exit" do
+	
+	puts text[para].text
+	doNext = gets.chomp
 
-puts text
+	if doNext == "exit"
+		para = "exit"
+	else
+		para += 1
+		
+			while text[para].text == ''
+				para += 1
+			end
+	end
+
+end
